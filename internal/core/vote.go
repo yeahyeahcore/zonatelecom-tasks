49bdb5f6ed17de048d38e6b3c184ed96f15a2f87
Kirill <kutuz.kirya.v.1.2.5@gmail.com>
2023-01-29 07:07:28 +0300

    refactor: voting state core

--- internal/core/vote.go
+++ internal/core/vote.go
@@ -7,17 +7,7 @@ type CreateVoteRequest struct {
 	Digest   string `json:"digest"`
 }
 
-type PreviousVotingState struct {
-	VotingID string            `json:"votingId"`
-	Results  []VoteStateResult `json:"results"`
-}
-
 type VotingStateOptionsMap struct {
 	VotingID string          `json:"votingId"`
 	Options  map[string]uint `json:"options"`
-}
-
-type VoteStateResult struct {
-	OptionID string `json:"optionId"`
-	Count    uint   `json:"count"`
 }
